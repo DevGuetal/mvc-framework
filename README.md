eb34f24a6d6a752678402312ebe18b47d263345d
DevGuetal <dev.guetal@gmail.com>
2015-03-16 19:50:57 +0000

    first working version

---
+++ README.md
@@ -0,0 +1,14 @@
+This is an exercise I had to do. Basically, I was given to build a framework similar to Angularjs, that would provide a basic data binding mechanism
+
+In order to view the result:
+
+* simply click on index.html or copy the files and run them in a server (for example Mongoose, targetting localhost:8080)
+* to see the 2 way data binding excercise, open the index_2way_data_binding.html
+
+Regarding the execution time, this exercise has taken me more time than what was specified in the instructions. I would say in total has taken 3 hours of work.
+
+
+
+
+
+
---
+++ js/Framework2WayDataBinding.js
@@ -0,0 +1,170 @@
+/**The framework */
+
+var Framework = function() {
+	/** PRIVATE MEMBERS */
+
+	var _templateList= [];	
+
+	var _extends = function (destination, source) {   
+		for (var property in source) {
+			destination[property] = source[property];
+		}
+		return destination;
+	};
+
+	// observer pattern for data binding /
+	var Observer = function () {
+		this._listeners = {};
+	};
+
+	Observer.prototype.notify = function (dataChanged, args) {
+		var subscribers, i;
+
+		subscribers = this._listeners[dataChanged];
+		if (subscribers !== undefined) {
+			for (i = 0; i < subscribers.length; i++) {
+				if(subscribers[i].context) {
+					subscribers[i].view.apply(subscribers[i].context, args);
+				} else {
+					subscribers[i].view(args);
+				} 
+			}
+		}
+	};
+
+	Observer.prototype.onDataChanged = function (dataChanged, view, ctx) {
+		var actionObj = this._listeners[dataChanged];
+		if (actionObj === undefined) {
+			this._listeners[dataChanged] = [
+			                                {context:ctx, view:view}
+			                                ];
+		}
+	};
+
+	//Creating a basic MVC pattern
+	//the model
+	var model = model || {};
+
+	//the view
+	var BaseTemplate = function() {
+		html = '';
+		placeholder = '';
+		ctrl = {};
+		observer = {};
+	};
+
+	BaseTemplate.prototype.refresh = function () {
+		var htmlToDisplay = this.html;
+
+		var items = htmlToDisplay.match(/{{(.*?)}}/g);
+		for(var i = 0; i < items.length; i++) {
+			var currentItem = items[i];
+			var key = currentItem.substring (2, currentItem.length - 2);
+
+			htmlToDisplay = htmlToDisplay.replace(currentItem, this.ctrl.getModel()[key]);
+
+			this.observer.onDataChanged(currentItem, this.refresh, this);
+		}
+
+		var lastFocusedElement = document.activeElement.id;
+		this.placeholder.innerHTML = htmlToDisplay;
+		
+		// we compute the html first, then we inject it in one time, in order to improve performances 
+		var newFocusableElement = document.getElementById(lastFocusedElement); 
+
+		//setting back the focus
+		if(newFocusableElement) {
+			newFocusableElement.focus();
+			newFocusableElement.selectionStart = newFocusableElement.selectionEnd = newFocusableElement.value.length
+		}
+	}
+
+	BaseTemplate.prototype.render = function (placeholder) {
+		this.placeholder = document.getElementsByClassName(placeholder.substring(1))[0];
+		this.refresh();
+	}
+
+	//the controller
+	var BaseController = function () {
+		data = {};	
+	};
+
+	BaseController.prototype.getModel = function () {
+		return this.data;
+	}
+
+	/** PUBLIC MEMBERS */
+	return {
+		create: function (args) {
+
+			template = args.template || {};
+			data     = args.data     || {};
+
+			var html = document.getElementById(template).innerHTML;
+
+			//sanitize html
+			html = html.replace(/(\r\n|\n|\r)/gm," ");
+			html = html.replace(/\s{2,}/g," ");
+			
+			//instanciating the controller
+			var ctrl = Object.create (BaseController.prototype);
+			ctrl = _extends(ctrl, args);
+			
+			// data binding
+			var dataObserver = Object.create (Observer.prototype);
+			dataObserver = _extends(dataObserver, new Observer());
+
+			var tpl = Object.create (BaseTemplate.prototype);
+			tpl = _extends (tpl, {
+						html: html,
+						ctrl: ctrl,
+						observer: dataObserver
+					});
+
+			//2 way data binding
+			var dataBindingItems = html.match(/data-bind="(.*?)"/g);
+
+			var eventObserver = Object.create (Observer.prototype);
+			_extends(eventObserver, new Observer());
+			
+			for(var i = 0; i < dataBindingItems.length; i++) {
+				var currentItem = dataBindingItems[i];
+				var events = currentItem.substring('data-bind="'.length, currentItem.length - 1);
+				
+				var callback = function(evt) {
+					if(evt) {
+						var target = evt.target || evt.srcElement;
+						
+						if (events && events !== "") {
+							ctrl.getModel()[events] = target.value;
+							dataObserver.notify("{{"+events+"}}");
+						}
+					}
+				}
+
+				eventObserver.onDataChanged('keyup', callback);
+			}
+			
+			// event binding
+//			var eventItems = html.match(/data-event="(.*?)"/g);
+//			for(var i = 0; i < eventItems.length; i++) {
+//				var currentItem = eventItems[i];
+//				var events = currentItem.substring('data-event="'.length, currentItem.length - 1).split(':');
+//
+//				eventObserver.onDataChanged(events[0], ctrl[events[1]]);
+//			}
+
+			// event delegation in order improve good performances
+			var _eventDelegate = function(evt) {
+				if (evt.target.tagName == 'INPUT') {
+					eventObserver.notify('keyup', evt);
+				}
+			}
+
+			document.addEventListener("keyup", _eventDelegate, false);
+
+			return tpl;	
+		}
+	}
+}();
+
